c845d184d9e5d1ca8b8be05e8764c3f051d0c83b
LuisRivera330 <coartac.luis.rivera@gmail.com>
2022-04-13 09:34:34 -0500

    Laboratorio02

--- Lab01/ejercicio02.js
+++
@@ -1,8 +0,0 @@
-let contador = 1;
-
-while(contador<=50){
-    if(contador % 7 == 0 && contador % 2 !== 0 && contador % 3 !== 0 && contador % 5 !== 0) {
-        console.log(contador);
-    }
-    contador = contador + 1;
-}
